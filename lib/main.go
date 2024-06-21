757cb8e9462b59e72a9c2d85d6809c685f747f2a
bradfordwagner <wagner.bradford@gmail.com>
2024-06-20 21:15:52 -0400

    create lib

---
+++ lib/main.go
@@ -0,0 +1,31 @@
+// A generated module for Lib functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+)
+
+type Lib struct{}
+
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *Lib) OpenConfigYaml(ctx context.Context, src Directory) (o string, err error) {
+	configFile := src.File("config.yaml")
+	bytes, err := configFile.Contents(ctx)
+	if err != nil {
+		return
+	}
+	return string(bytes), nil
+}
