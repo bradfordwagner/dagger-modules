aa01b68608e8398dce3d86c5fd68174c7be98590
bradfordwagner <wagner.bradford@gmail.com>
2024-09-21 23:10:07 -0400

    default path

--- lib/main.go
+++ lib/main.go
@@ -24,7 +24,11 @@ import (
 type Lib struct{}
 
 // Returns lines that match a pattern in the files of the provided Directory
-func (m *Lib) OpenConfigYaml(ctx context.Context, src dagger.Directory) (s string, err error) {
+func (m *Lib) OpenConfigYaml(
+	ctx context.Context,
+	// +defaultPath="/"
+	src dagger.Directory,
+) (s string, err error) {
 	configFile := src.File("config.yaml")
 	return configFile.Contents(ctx)
 }
